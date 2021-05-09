284ceb294c0fb4224535a0f6278db36f04554043
Mateo Velasquez-Giraldo <mateovegi@gmail.com>
2021-05-09 15:24:08 -0400

    First version of infinite horizon

--- Code/Python/Simulations/example_Inf_ConsRiskyContribModel.py
+++ Code/Python/Simulations/example_Inf_ConsRiskyContribModel.py
@@ -1,8 +1,11 @@
 # %%
-'''
+"""
 Example implementations of HARK.ConsumptionSaving.ConsPortfolioModel
-'''
-from HARK.ConsumptionSaving.ConsRiskyAssetModel import RiskyContribConsumerType, init_risky_contrib
+"""
+from HARK.ConsumptionSaving.ConsRiskyAssetModel import (
+    RiskyContribConsumerType,
+    init_risky_contrib,
+)
 from time import time
 from copy import copy
 
@@ -15,26 +18,24 @@ import os
 
 # Make the problem infinite horizon
 par_infinite_base = init_risky_contrib.copy()
-par_infinite_base['cycles']   = 0
+par_infinite_base["cycles"] = 0
 
 # and frictionless to start
-par_infinite_base['AdjustPrb'] = 1.0
-par_infinite_base['tau'] = 0.0
+par_infinite_base["AdjustPrb"] = 1.0
+par_infinite_base["tau"] = 0.0
 
 # Make contribution shares a continuous choice
-par_infinite_base['DiscreteShareBool'] = False
-par_infinite_base['vFuncBool'] = False
+par_infinite_base["DiscreteShareBool"] = False
+par_infinite_base["vFuncBool"] = False
 
 # Temporarily make grids sparser
-par_infinite_base.update({
-    "aXtraCount": 30,
-    "mNrmCount": 30,
-    "nNrmCount": 30,
-})
+par_infinite_base.update(
+    {"aXtraCount": 30, "mNrmCount": 30, "nNrmCount": 30,}
+)
 
 # %% A version with the tax
 par_infinite_tax = copy(par_infinite_base)
-par_infinite_tax['tau'] = 0.1
+par_infinite_tax["tau"] = 0.1
 
 # %% A version with the Calvo friction
 par_infinite_calvo = copy(par_infinite_base)
@@ -42,20 +43,26 @@ par_infinite_calvo["AdjustPrb"] = 0.25
 
 # %% Create and solve agents with all the parametrizations
 
-agents = {'Base': RiskyContribConsumerType(**par_infinite_base),
-          'Tax': RiskyContribConsumerType(**par_infinite_tax),
-          'Calvo': RiskyContribConsumerType(**par_infinite_calvo)}
+agents = {
+    "Base": RiskyContribConsumerType(**par_infinite_base),
+    "Tax": RiskyContribConsumerType(**par_infinite_tax),
+    "Calvo": RiskyContribConsumerType(**par_infinite_calvo),
+}
 
 for agent in agents:
-    
-    print('Now solving ' + agent)
+
+    print("Now solving " + agent)
     t0 = time()
-    agents[agent].solve(verbose = True)
+    agents[agent].solve(verbose=True)
     t1 = time()
-    print('Solving ' + agent +' took ' + str(t1-t0) + ' seconds.')
+    print("Solving " + agent + " took " + str(t1 - t0) + " seconds.")
 
 # %% Hark plot setup
-from HARK.utilities import find_gui, make_figs, determine_platform, test_latex_installation, setup_latex_env_notebook
+from HARK.utilities import (
+    determine_platform,
+    test_latex_installation,
+    setup_latex_env_notebook,
+)
 
 pf = determine_platform()
 try:
@@ -71,50 +78,69 @@ saveFigs = True
 # Whether to draw the figures
 drawFigs = True
 
+
 def make(fig, name, target_dir="../../../Figures"):
     fig.savefig(os.path.join(target_dir, "{}.pdf".format(name)))
+
 
 # %% Plots
 
 
 t = 0
-mNrmGrid = np.linspace(0,50,100)
-nNrm_vals = np.array([0.0, 10.0, 20])
+mNrmGrid = np.linspace(0, 40, 100)
+nNrm_vals = np.array([0.0, 20.0, 40])
 Share_vals = np.array([0.0, 0.5])
 
 polfuncs = pol_funcs_dframe(agents, t, mNrmGrid, nNrm_vals, Share_vals)
 
 # General aesthetics
-
-#sns.set(rc={'figure.figsize':(2,2)})
 sns.set_style("whitegrid")
-sns.set_context("paper",
-                font_scale=1.5,
-                rc={"lines.linewidth": 2.5})
+sns.set_context("paper", font_scale=1.5, rc={"lines.linewidth": 2.5})
 
 
 # Rebalancing fraction
-g = sns.FacetGrid(polfuncs[polfuncs.control == "d"], col="n", hue = "model",
-                  height=3, aspect=(7/3)*1/3)
-g.map(sns.lineplot, "m", "value", alpha=.7, linewidth = 2)
-g.add_legend(bbox_to_anchor=[0.5, 0.0], ncol = 3, title = "")
-g.set_axis_labels('m', 'Normalized Rebalancing Flow: d')
-make(g,'inf_dFunc')
+g = sns.FacetGrid(
+    polfuncs[polfuncs.control == "d"],
+    col="n",
+    hue="model",
+    height=3,
+    aspect=(7 / 3) * 1 / 3,
+)
+g.map(sns.lineplot, "m", "value", alpha=0.7)
+g.add_legend(bbox_to_anchor=[0.5, 0.0], ncol=3, title="")
+g.set_axis_labels("$m$", "Rebalancing Flow: $d$")
+make(g, "inf_dFunc")
+
+# After rebalancing, m and n turn to their "tilde" versions. Create ntilde
+# just for seaborn's grid labels.
+polfuncs["$\\tilde{n}$"] = polfuncs["n"]
+polfuncs["$\\Contr$"] = polfuncs["Share"]
 
 # Share fraction
-g = sns.FacetGrid(polfuncs[polfuncs.control == "Share"], col="n", hue = "model",
-                  height=3, aspect=(7/3)*1/3)
-g.map(sns.lineplot, "m", "value", alpha=.7, linewidth = 2)
-g.add_legend(bbox_to_anchor=[0.5, 0.0], ncol = 3, title = "")
-g.set_axis_labels(r'$\tilde{m}$', 'Income Deduct. Share')
+g = sns.FacetGrid(
+    polfuncs[polfuncs.control == "Share"],
+    col="$\\tilde{n}$",
+    hue="model",
+    height=3,
+    aspect=(7 / 3) * 1 / 3,
+)
+g.map(sns.lineplot, "m", "value", alpha=0.7)
+g.add_legend(bbox_to_anchor=[0.5, 0.0], ncol=3, title="")
+g.set_axis_labels("$\\tilde{m}$", r"Deduction Share: $\Contr$")
 
-make(g,'inf_ShareFunc')
+make(g, "inf_ShareFunc")
 
 # Consumption fraction
-g = sns.FacetGrid(polfuncs[polfuncs.control == "c"], col="n", row = "Share", hue = "model",
-                  height=3, aspect=(7/3)*1/3)
-g.map(sns.lineplot, "m", "value", alpha=.7, linewidth = 2)
-g.add_legend(bbox_to_anchor=[0.5, 0.0], ncol = 3, title = "")
-g.set_axis_labels(r'$\tilde{m}$', r'Consumption: $c$')
+g = sns.FacetGrid(
+    polfuncs[polfuncs.control == "c"],
+    col="$\\tilde{n}$",
+    row="$\\Contr$",
+    hue="model",
+    height=3,
+    aspect=(7 / 3) * 1 / 3,
+)
+g.map(sns.lineplot, "m", "value", alpha=0.7)
+g.add_legend(bbox_to_anchor=[0.5, 0.0], ncol=3, title="")
+g.set_axis_labels("$\\tilde{m}$", "Consumption: $c$")
 
-make(g,'inf_cFunc')
+make(g, "inf_cFunc")
