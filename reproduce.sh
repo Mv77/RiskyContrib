5e13e4b71c53786d5fb9dfa60600a55f8ad9bebc
Mateo Velasquez-Giraldo <mateovegi@gmail.com>
2021-07-15 15:04:07 -0400

    Simplify reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -3,9 +3,5 @@ source /opt/conda/etc/profile.d/conda.sh
 mamba env create -qq -f environment.yml
 conda activate RiskyContrib
 
-# Then pull a version of HARK from GitHub. It has not been
-# released yet
-#pip install git+git://github.com/econ-ark/HARK
-
 # execute the script to create figures
 ipython do_ALL.py
