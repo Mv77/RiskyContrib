31338a39b05d7e7b54f5b329d8ae5079f253cda8
Mateo Velasquez-Giraldo <mateovegi@gmail.com>
2021-07-15 11:50:19 -0400

    Initial nbreproduce setup

--- reproduce.sh
+++ reproduce.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
+# requirements setup and mamba for faster install for env
+source /opt/conda/etc/profile.d/conda.sh
+mamba env create -qq -f environment.yml
+conda activate RiskyContrib
 
-python3 -m pip install -r ./binder/requirements.txt
-python3 ./do_ALL.py
+# Then pull a version of HARK from GitHub. It has not been
+# released yet
+pip install git+git://github.com/econ-ark/HARK
+
+# execute the script to create figures
+ipython do_ALL.py
